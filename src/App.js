8c917ad162dd1ca3985a363dde4073b3878b151d
pinus0711 <yyis3173@gmail.com>
2023-05-03 03:17:37 +0900

    [CHORE]css파일 분리 및 코드 수정

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        {<Navbar />}
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/diary" element={<Diary />}></Route>
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -38,8 +38,6 @@ const Diary = () => {
   return (
     <div className="Diary">
       <div className="DiaryFrame">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         <div className="DiaryImageContainer">
           <img
             className="DiaryImage"
@@ -76,9 +74,9 @@ const Diary = () => {
               />
             </div>
           </div>
-=======
-=======
->>>>>>> Stashed changes
+          <IndexBtn />
+        </div>
+
         <div className="LeftDivOveray">
           <div className="Left">
             <header>
@@ -92,12 +90,20 @@ const Diary = () => {
             <BottomBtn text={"하단버튼2"}></BottomBtn>
           </div>
         </div>
-        <div className="Right">
-          <IndexBtn />
-          <div className="textsquare">{squares}</div>
->>>>>>> Stashed changes
+        <div className="RightDivOveray">
+          <div className="Right">
+            <header>
+              <div className="title">제목: </div>
+              <div className="작성자">오늘의 기분</div>
+            </header>
+
+            <div className="TextSquareContainer">{squares}</div>
+            <div className="RightBottomDiv">
+              <BottomBtn text={"수정하기"}></BottomBtn>
+              <BottomBtn text={"삭제하기"}></BottomBtn>
+            </div>
+          </div>
         </div>
-        <div className="TextSquareContainer">{squares}</div>
       </div>
     </div>
   );
