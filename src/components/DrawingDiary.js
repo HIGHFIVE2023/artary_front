8e06a15f9a40e21ce7800c8f21071d469a815cbd
pinus0711 <yyis3173@gmail.com>
2023-05-09 01:27:43 +0900

    [CHORE]그림일기 컴포넌트 분리

---
+++ src/components/DrawingDiary.js
@@ -0,0 +1,12 @@
+const DrawingDiary = () => {
+  return (
+    <div className="Left">
+      <header>
+        <div className="theDate">날짜</div>
+        <div className="Emotion">오늘의 기분</div>
+        <div className="todayEmotion">이모지</div>
+      </header>
+    </div>
+  );
+};
+export default DrawingDiary;
