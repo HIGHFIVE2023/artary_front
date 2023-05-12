fdb9e8cf986f9f5efd4aca87864691784d98db1c
wi5h <114387225+wi5h@users.noreply.github.com>
2023-05-12 13:28:48 +0900

    [FIX] 인덱스버튼 css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import New from "./pages/New";
-=======
-import Navbar from "./components/Navbar";
->>>>>>> 8e06a15f9a40e21ce7800c8f21071d469a815cbd
 
 function App() {
   return (
