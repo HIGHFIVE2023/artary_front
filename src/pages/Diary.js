639ac2e6dd78d0d3dedeb7b1740ac9e525e6486f
wi5h <114387225+wi5h@users.noreply.github.com>
2023-05-16 23:45:34 +0900

    [ADD] 다이어리 컴포넌트 분리

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -2,39 +2,9 @@ import React from "react";
 import IndexBtn from "../components/IndexBtn";
 import BottomBtn from "../components/BottomBtn";
 import DrawingDiary from "../components/DrawingDiary";
+import WritingDiary from "../components/WritingDiary";
 
 const Diary = () => {
-  const str = "";
-
-  const numRows = 9;
-  const numCols = 8;
-  const squares = [];
-
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numCols; j++) {
-      const idx = i * numCols + j;
-      const char = idx < str.length ? str[idx] : "";
-      squares.push(<TextSquare key={idx} text={char} />);
-    }
-  }
-
-  function TextSquare({ text }) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "35px",
-          height: "35px",
-          border: "1px solid black",
-        }}
-      >
-        {text}
-      </div>
-    );
-  }
-
   return (
     <div className="Diary">
       <div className="DiaryFrame">
@@ -56,17 +26,10 @@ const Diary = () => {
           </div>
         </div>
         <div className="RightDivOveray">
-          <div className="Right">
-            <header>
-              <div className="title">제목: </div>
-              <div className="작성자">오늘의 기분</div>
-            </header>
-
-            <div className="TextSquareContainer">{squares}</div>
-            <div className="RightBottomDiv">
-              <BottomBtn text={"수정하기"}></BottomBtn>
-              <BottomBtn text={"삭제하기"}></BottomBtn>
-            </div>
+          <WritingDiary />
+          <div className="RightBottomDiv">
+            <BottomBtn text={"수정하기"}></BottomBtn>
+            <BottomBtn text={"삭제하기"}></BottomBtn>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import IndexBtn from "../components/IndexBtn";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,23 +24,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <nav>
-        <button onClick={toggleMenu}>메뉴</button>{" "}
-        {/* 버튼 클릭 시 toggleMenu 함수 호출 */}
-        <ul className={isOpen ? "show" : "hide"}>
-          {" "}
-          {/* isOpen 상태에 따라 클래스명 설정 */}
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
     </div>
   );
 };
