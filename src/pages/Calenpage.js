4305a2678871aeb92fab33bd86c8d84ad80c7fd5
pinus0711 <yyis3173@gmail.com>
2023-07-18 23:39:10 +0900

    [FEAT]캘린더 이모지 불러오기

--- src/pages/Calenpage.js
+++ src/pages/Calenpage.js
@@ -1,23 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import moment from "moment/moment";
+import moment from "moment";
 import IndexBtn from "../components/IndexBtn";
 import Chart from "./Chart";
+import { useParams } from "react-router";
+import { call } from "../service/ApiService";
+import axios from "axios";
 
 function Calenpage() {
   const [date, setDate] = useState(new Date());
-  const mark = ["2023-05-02"];
+  const [mark, setMark] = useState([]);
+
+  const emotionImg = {
+    HAPPY: "emotion01.png",
+    SOSO: "emotion02.png",
+    SAD: "emotion03.png",
+    ANGRY: "emotion04.png",
+  };
+
+  useEffect(() => {
+    call(`/diary/diaries`, "GET", null)
+      .then((response) => {
+        console.log(response);
+        const markList = response.map((diary) => {
+          return {
+            date: diary.createdAt.substring(0, 10),
+            emotion: diary.emotion, // 이모지 데이터가 어떤 프로퍼티에 저장되어 있는지에 따라 변경
+          };
+        });
+        setMark(markList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // 각 날짜의 타일에 이모지를 표시하는 함수
+  const tileContent = ({ date, view }) => {
+    const formattedDate = moment(date).format("YYYY-MM-DD");
+    const emojiData = mark.find((item) => item.date === formattedDate);
+
+    return (
+      <div>
+        {emojiData && (
+          <div className="emotion_img_wrapper">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `../img/${emotionImg[emojiData.emotion]}`
+              }
+              alt={`Emoji for ${emojiData.emotion}`} // 이미지에 대한 설명 추가 (웹 접근성을 위해)
+              className="emoji-image"
+            />
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="Diary">
       <div className="DiaryFrame">
         <div className="DiaryImageContainer">
-          <img
-            className="DiaryImage"
-            src="/img/diary.png"
-            alt="diary background"
-          />
           <div className="IndexBtnContainer">
             <IndexBtn type={"calendar"} text2={"캘린더"} />
           </div>
@@ -26,24 +71,9 @@ function Calenpage() {
             formatDay={(locale, date) => moment(date).format("DD")}
             value={date}
             locale="ko-KO"
+            calendarType="US"
             className="CalendarPage"
-            tileContent={({ date, view }) => {
-              // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-              // 추가할 html 태그를 변수 초기화
-              let html = [];
-              // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
-              if (mark.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
-                html.push(<div className="dot"></div>);
-              }
-              // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
-              return (
-                <>
-                  <div className="flex justify-center items-center absoluteDiv">
-                    {html}
-                  </div>
-                </>
-              );
-            }}
+            tileContent={tileContent} // 이모지를 표시하는 tileContent 함수를 전달
           />
           <div className="LeftDivOveray">
             <Chart />
