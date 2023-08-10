1a1bb15fd83b19e444ca9c08074ec9a270230b67
pinus0711 <yyis3173@gmail.com>
2023-08-11 00:20:17 +0900

    [FIX]친구관리코드 수정

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { call } from "../service/ApiService";
-import { UserOutlined } from '@ant-design/icons';
-import { Avatar} from 'antd';
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
 
 const FriendsList = () => {
-  const defaultImageURL = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+  const defaultImageURL =
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
   const [friends, setFriends] = useState([]);
 
   useEffect(() => {
     call(`/friend`, "GET", null)
       .then((response) => {
-        setFriends(response);
+        console.log(response);
+        const friendList = response.map((f) => {
+          return {
+            nickname: f.nickname,
+            email: f.email,
+            image: f.image,
+          };
+        });
+        setFriends(friendList);
       })
       .catch((error) => {
         console.log(error);
@@ -20,34 +29,47 @@ const FriendsList = () => {
   const deleteFriend = useCallback((friendEmail) => {
     const confirmed = window.confirm("정말로 친구를 삭제하시겠습니까?");
     if (confirmed) {
-      call(`/friend/${friendEmail}`, 'DELETE', null)
-      .then(() => {
-        setFriends((prevfFriends) => prevfFriends.filter((friend) => friend.email !== friendEmail));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      call(`/friend/${friendEmail}`, "DELETE", null)
+        .then(() => {
+          setFriends((prevfFriends) =>
+            prevfFriends.filter((friend) => friend.email !== friendEmail)
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-    
   }, []);
 
   return (
-    <div>
-      <ul>
+    <div
+      className="user-list-container"
+      style={{ height: "80%", overflowY: "auto" }}
+    >
+      <ul className="user-list">
         {friends.map((friend, index) => {
-          const profileImage = friend.image !== null ? friend.image : defaultImageURL;
+          const profileImage =
+            friend.image !== null ? friend.image : defaultImageURL;
           return (
-            <li key={index}>
-              <Avatar size={40} style={{margin:'20px'}}  icon={<UserOutlined />} src={profileImage} />
-              <p>{friend.nickname}</p>
-              <p>{friend.email}</p>
-              <button onClick={() => deleteFriend(friend.email)}>삭제</button>
+            <li key={index} className="user-item">
+              <Avatar
+                size={40}
+                style={{ margin: "20px" }}
+                icon={<UserOutlined />}
+                src={profileImage}
+              />
+              <div className="user-info">
+                <p className="username">{friend.nickname}</p>
+                <p className="email">{friend.email}</p>
+
+                <button onClick={() => deleteFriend(friend.email)}>삭제</button>
+              </div>
             </li>
           );
         })}
       </ul>
     </div>
-  )
+  );
 };
 
 export default FriendsList;
