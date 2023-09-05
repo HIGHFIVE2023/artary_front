ef232b2651b8a5112b9632ec19939804acb5cb14
pinus0711 <yyis3173@gmail.com>
2023-09-05 22:01:46 +0900

    [FIX]도장수정

--- src/components/SelectStamp.js
+++ src/components/SelectStamp.js
@@ -1,26 +1,24 @@
-import React, { useState } from "react";
-import { call } from "../service/ApiService";
+import React, { useEffect, useState } from "react";
+import { call, getSticker } from "../service/ApiService";
 
 const SelectStamp = ({ diaryId, onClose }) => {
-  const [stickerId, setStickerId] = useState("");
+  const createButtonClickHandler = (buttonType) => async () => {
+    try {
+      // 보낼 데이터
+      const data = { type: buttonType };
 
-  const createButtonClickHandler = (buttonType) => () => {
-    // 보낼 데이터
-    const data = { type: buttonType };
+      // API 호출 전에 전달되는 데이터, diaryId 확인
+      console.log("Button Clicked:", buttonType);
+      console.log("diaryId:", diaryId);
 
-    // API 호출 전에 전달되는 데이터, diaryId 확인
-    console.log("Button Clicked:", buttonType);
-    console.log("diaryId:", diaryId);
+      const response = await call(`/diary/${diaryId}/sticker`, "POST", data);
 
-    call(`/diary/${diaryId}/sticker`, "POST", data)
-      .then((response) => {
-        console.log("API Response:", response);
-        setStickerId(response);
-        alert(buttonType + " SUCCESS!");
-      })
-      .catch((error) => {
-        console.log("API Error:", error);
-      });
+      console.log("API Response:", response);
+      alert(buttonType + " SUCCESS!");
+      onClose();
+    } catch (error) {
+      console.log("API Error:", error);
+    }
   };
   return (
     <div className="selectStamp">
@@ -51,7 +49,6 @@ const SelectStamp = ({ diaryId, onClose }) => {
           <img src="../img/perfect.PNG" />
         </button>
       </div>
-
       <button onClick={onClose}>닫기</button>
     </div>
   );
