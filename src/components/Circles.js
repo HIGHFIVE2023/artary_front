d0ad9656d5d7f962ceaae2bc7d905f35c1862021
wi5h <114387225+wi5h@users.noreply.github.com>
2023-09-28 12:24:05 +0900

    [FIX] intro, 작성페이지

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 const CircleContainer = styled.div`
   display: flex;
+  height: 100%;
   flex-direction: column;
   align-items: center;
   alignself: flex-start;
   z-index: 1;
-  margin-top: 40%;
 `;
 
 const Circle = styled.div`
@@ -15,17 +15,29 @@ const Circle = styled.div`
   width: 1.5em;
   height: 1.5em;
   border-radius: 50%;
-  margin-bottom: 3em;
+  margin-bottom: auto;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25) inset;
 `;
 
 const Circles = ({ count, style }) => {
   // count 개수만큼 Circle 컴포넌트 렌더링
   const circleElements = Array.from({ length: count }, (_, index) => (
-    <Circle key={index} />
+    <Circle
+      key={index}
+      style={
+        index === 0
+          ? { ...style, marginBottom: "auto" } // 첫 번째 원에는 아래쪽 마진과 위쪽 마진을 적용
+          : index === count - 1
+          ? {
+              ...style,
+              marginTop: "auto",
+            } // 마지막 원에는 위쪽 마진과 아래쪽 마진을 적용
+          : { ...style, margin: "auto" } // 중간 원들에는 상하 마진을 없애고 다른 스타일 적용
+      }
+    />
   ));
 
-  return <CircleContainer style={style}>{circleElements}</CircleContainer>;
+  return <CircleContainer>{circleElements}</CircleContainer>;
 };
 
 export default Circles;
--- src/components/Springs.js
+++ src/components/Springs.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 
 const SpringContainer = styled.div`
   display: flex;
+  height: 100%;
   flex-direction: column;
   align-items: center;
   alignself: flex-start;
-  margin-top: 51%;
 `;
 
 const Spring = styled.div`
@@ -15,17 +15,26 @@ const Spring = styled.div`
   border: 7px solid #adb5bd;
   border-bottom: none;
   border-radius: 50% / 100% 100% 0% 0%;
-  margin-bottom: 3.1em;
+  margin-bottom: auto;
   z-index: 2;
 `;
 
 const Springs = ({ count, style }) => {
   // count 개수만큼 Spring 컴포넌트 렌더링
   const springElements = Array.from({ length: count }, (_, index) => (
-    <Spring key={index} />
+    <Spring
+      key={index}
+      style={
+        index === 0
+          ? { ...style, marginBottom: "auto" } // 첫 번째 스프링에는 아래쪽 마진과 위쪽 마진을 적용
+          : index === count - 1
+          ? { ...style, marginTop: "auto" } // 마지막 스프링에는 위쪽 마진과 아래쪽 마진을 적용
+          : { ...style, margin: "auto" } // 중간 스프링들에는 상하 마진을 없애고 다른 스타일 적용
+      }
+    />
   ));
 
-  return <SpringContainer style={style}>{springElements}</SpringContainer>;
+  return <SpringContainer>{springElements}</SpringContainer>;
 };
 
 export default Springs;
